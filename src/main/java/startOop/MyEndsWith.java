008ef1b000bebae2623d8c31a8352478a8f8ec20
Андрей <i.popolin@gmail.com>
2017-11-04 01:06:18 +0200

    new class

--- src/main/java/startOop/MyEndsWith.java
+++ src/main/java/startOop/MyEndsWith.java
@@ -4,6 +4,27 @@ public class MyEndsWith {
 
     public static void main(String[] args) {
 
+        System.out.println(myEndsWith("Пробуем проверит окончание предложения", "предложения"));
+    }
 
+    private static boolean myEndsWith(String line1, String line2) {
+        char checkString[] = new char[line1.length()];
+        char[] endLine = line2.toCharArray();
+        int counter = 0;
+        for (int i = line1.length() - 1; i >= 0; i--) {
+            for (int j = endLine.length - 1; j >= 0; j--) {
+                if (endLine[j] == line1.charAt(i)) {
+                    counter++;
+                    i--;
+                    if (counter == line2.length()) {
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
+
+            }
+        }
+        return false;
     }
 }

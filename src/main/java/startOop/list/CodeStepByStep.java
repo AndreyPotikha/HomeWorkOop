f1c8603795e3194c5b771745189273b2350eb8f3
Андрей <i.popolin@gmail.com>
2017-11-05 23:40:49 +0200

    new class

--- src/main/java/startOop/list/CodeStepByStep.java
+++ src/main/java/startOop/list/CodeStepByStep.java
@@ -1,14 +1,19 @@
 package startOop.list;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.awt.image.SampleModel;
+import java.util.*;
 
 public class CodeStepByStep {
 
     public static void main(String[] args) {
 
-        Twice();
+        deleteDuplicates();
+
+//        RemoveRange(2, 6);
+
+//        Repeat(4);
+
+//        Twice();
 
 //        SwitchPairs();
 
@@ -30,6 +35,70 @@ public class CodeStepByStep {
         removeEvenLength(remove);*/
 
 //        arrayListMysteryInsert();
+    }
+
+    private static void deleteDuplicates() {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("be");
+        list.add("be");
+        list.add("is");
+        list.add("not");
+        list.add("or");
+        list.add("or");
+        list.add("thet");
+        list.add("the");
+        list.add("to");
+        list.add("to");
+        Set<String> setLine = new HashSet<String>();
+        setLine.addAll(list);
+        System.out.println(setLine);
+    }
+
+// Write a method named removeRange that accepts an ArrayList of integers and two integer values min and max
+// as parameters and removes all elements values in the range min through max (inclusive).
+// For example, if an ArrayList named list stores [7, 9, 4, 2, 7, 7, 5, 3, 5, 1, 7, 8, 6, 7],
+// the call of removeRange(list, 5, 7); should change the list to store [9, 4, 2, 3, 1, 8].
+    private static void RemoveRange(Integer min, Integer max) {
+        ArrayList<Integer> removeMinMax = new ArrayList<Integer>();
+        removeMinMax.add(1);
+        removeMinMax.add(2);
+        removeMinMax.add(4);
+        removeMinMax.add(5);
+        removeMinMax.add(6);
+        removeMinMax.add(2);
+        removeMinMax.add(4);
+        removeMinMax.add(3);
+        removeMinMax.add(4);
+        removeMinMax.add(6);
+        removeMinMax.add(5);
+        for (int i = 0; i < removeMinMax.size() - 1; i++) {
+            if (removeMinMax.get(i) == min || removeMinMax.get(i) == max) {
+                removeMinMax.remove(removeMinMax.get(i));
+            }
+        }
+        System.out.println(removeMinMax);
+    }
+
+// Write a method named repeat that accepts an ArrayList of Strings and an integer k as parameters
+// and that replaces each element with k copies of that element. For example, if the list stores the values
+// ["how", "are", "you?"] before the method is called and k is 4, it should store the values
+// ["how", "how", "how", "how", "are", "are", "are", "are", "you?", "you?", "you?", "you?"]
+// after the method finishes executing. If k is 0 or negative, the list should be empty after the call.
+    private static void Repeat(int count) {
+        ArrayList<String> repeator = new ArrayList<String>();
+        repeator.add("how");
+        repeator.add("are");
+        repeator.add("you");
+        if (count == 0) {
+            repeator.clear();
+        } else {
+            for (int i = 0; i < repeator.size(); i += count) {
+                for (int j = 1; j < count; j++) {
+                    repeator.add(i, repeator.get(i));
+                }
+            }
+        }
+        System.out.println(repeator);
     }
 
     private static void Twice() {
